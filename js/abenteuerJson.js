3cbf1c9584ec314123559507a5323f8bbea725d8
codepec <89002954+codepec@users.noreply.github.com>
2021-08-17 08:20:54 +0200

    Add files via upload

---
+++ js/abenteuerJson.js
@@ -0,0 +1,103 @@
+var geschichte ={
+	situationen:[
+
+	{
+		id:0,
+		auswahlText: "Erwarte die Entscheidung",
+		"text":
+		"Deine Reise ist hier zu Ende. <br> Versuche es erneut. <br> GAME OVER",
+		ziele:[1]
+		
+		
+		
+	},
+
+
+	
+	{
+		id:1,
+		auswahlText: "Gehe zum Dorfplatz",
+		"text":
+		"Auf dem Dorfplatz wird gerade ein riesiges Fest gefeiert. Mitten im Hochsommer brennt die Sonne bei blauem Himmel auf die ausgelassen feiernde Dorfgemeinschaft nieder. Du setzt dich auf eine Parkbank, welche unter einer grossen Linde neben einem kleinen Bach liegt und geniesst das Fest. Eine Bedienung bringt dir auch sofort ein kühles Bier. Die Liveband und das Bier bringen dich immer mehr in Feierlaune. Du verlässt dein schattiges Plätzchen und begibst dich ebenfalls auf die Tanzfläche. Einige Stunden später musst du dich entscheiden..",
+		ziele:[2,3]
+		
+		
+		
+	},
+	
+		{
+		id:2,
+		auswahlText: "Verlasse die Feier und das Dorf",
+		"text":
+		"Du fühlst dich durch das viele Bier und die zahlreichen Drehungen etwas angeheitert und bist hin und hergerissen, ob du nicht doch bleiben hättest sollen. Plötzlich ruft dir eine zauberhafte Stimme nach doch noch ein wenig zu bleiben.",
+		ziele:[7]
+		
+		
+		
+	},
+	
+		{
+		id:3,
+		auswahlText: "Ziehe mit deinen neuen Freunden weiter ins Wirtshaus",
+		"text":
+		"Angeregt durch die positive Stimmung, welche im ganzen Dorf herrscht. Begibst du dich mit den anderen feierwütigen ins nächstgelegene Wirtshaus. Dort stellt dir der Wirt auch sofort ein weiteres kühles Getränk vor die Nase. Du kippst es mit nur einem Zug deine durstige Kehle hinunter. Nach so einem Liter Bier musst du plötzlich ganz schnell auf die Toilette",
+		ziele:[4]
+		
+		
+		
+	},
+	
+		{
+		id:4,
+		auswahlText: "Stürme zur Toilette",
+		"text":
+		"Bei der Toilette angelangt, merkst du plötzlich wie dir schwarz vor Augen wird...",
+		ziele:[5,6]
+		
+		
+		
+	},
+	
+			{
+		id:5,
+		auswahlText: "Versuche dich in die Toilette zu übergeben",
+		"text":
+		"Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text ",
+		ziele:[1]
+		
+		
+		
+	},
+	
+	
+	
+
+	{
+		id:6,
+		auswahlText: "Schleppe dich mit letzter Kraft in den Putzraum",
+		"text":
+		"Gerade so hast du es noch geschafft dich in den Putzraum einzuschliessen. Bevor alles um dich herum dunkel wurde",
+		ziele:[7]
+		
+		
+		
+	},		
+	
+				{
+		id:7,
+		auswahlText: "Drehe dich zur schönen Stimme",
+		"text":
+		"Du drehst dich um und genau in jenem Moment erkennst du die lauernde Gefahr. Von allen Seiten strömen zahlreiche Ratten auf dich zu. Ratten mit feuerroten Augen. Du zückst gerade noch dein Schwert. Doch, es werden immer mehr und die schöne Stimme verwandelt sich in schallendes Gelächter...",
+		ziele:[0]
+		
+		
+		
+	}
+	
+	]
+	
+	
+	
+	
+	
+};
